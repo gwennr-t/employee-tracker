a550fbee1cbce7ec1478aaca3067f4925cdbfe50
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-14 12:55:08 -0500

    made updates to set up mysql

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const mysql = require('mysql2');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'insert password here',
+    database: 'company_db'
+  },
+  console.log(`Connected to the company_db database.`)
+);
 
 // read all departments
 app.get('/api/department', (req, res) => {
@@ -135,6 +149,6 @@ app.use((req, res) => {
   res.status(404).end();
 });
   
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
